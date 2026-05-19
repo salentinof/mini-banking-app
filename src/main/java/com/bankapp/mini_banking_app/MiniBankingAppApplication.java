a677d09c28f2c6e639a537b2ff159f5006a710c3
frah <football57dt@gmail.com>
2026-05-19 18:55:44 +0200

    Initial commit

---
+++ src/main/java/com/bankapp/mini_banking_app/MiniBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.bankapp.mini_banking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBankingAppApplication.class, args);
+	}
+
+}
